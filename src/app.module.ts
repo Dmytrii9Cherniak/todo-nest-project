3e2d2c4245a0fe816d393aa9acca219b8643d4f1
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2023-03-01 08:44:44 +0200

    added controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TasksModule } from "./modules/tasks.module";
+import { UsersModule } from "./modules/users.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TasksModule, UsersModule],
 })
 export class AppModule {}
